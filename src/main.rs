3643552fb0e067104ae1ec46aeb6bbc5ddda84ad
Zach Bochanski <ztbochanski@gmail.com>
2024-01-05 16:42:29 -0800

    add buf reader trait and prelude to read and write from the stream
    this is completed in the handle connection method, lines are added as strings to the http request vector

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+}
 
 fn main() {
-    let listener = TcpListener::bind("127:0:0:1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer_reader = BufReader::new(&mut stream);
+    let mut http_request: Vec<_> = buffer_reader
+        .lines()
+        .map(|line| line.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request: {:?}", http_request);
 }
