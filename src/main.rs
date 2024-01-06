c974570e78a7e91c6fe40a6cb35b5fd636334d89
Zach Bochanski <ztbochanski@gmail.com>
2024-01-05 16:50:42 -0800

    add response by writing an ok status to the stream in handle connection method

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-}
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -14,11 +14,14 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer_reader = BufReader::new(&mut stream);
-    let mut http_request: Vec<_> = buffer_reader
+    let buffer_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buffer_reader
         .lines()
         .map(|line| line.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    println!("Request: {:?}", http_request);
+    
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
